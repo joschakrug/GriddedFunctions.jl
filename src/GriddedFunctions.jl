9951b34c86c9140d8f2796a329b6b35b4db887cc
Joscha Krug <joscha.krug@proton.me>
2026-03-02 21:54:51 -0800

    Exported GFInterpolation

--- src/GriddedFunctions.jl
+++ src/GriddedFunctions.jl
@@ -7,6 +7,6 @@ include("grids.jl")
 include("functions.jl")
 
 export Axis, LinearAxis, DiscreteAxis, Grid
-export GriddedFunction, interpolate, xmap, xmap!, points, maxpoint
+export GriddedFunction, GFInterpolation, interpolate, xmap, xmap!, points, maxpoint
 
 end # module GriddedFunctions
