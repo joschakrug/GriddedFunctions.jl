29b61c36a57bc0cf0c0ff4a95f8e3d4799ee0d79
Joscha Krug <joscha.krug@proton.me>
2026-03-09 16:18:06 -0700

    Improved public API

--- src/GriddedFunctions.jl
+++ src/GriddedFunctions.jl
@@ -8,7 +8,7 @@ include("functions.jl")
 include("views.jl")
 
 export Axis, LinearAxis, DiscreteAxis, AbstractGrid, Grid, gridaxes, inbounds
-export GriddedFunction, GFInterpolation
+export AbstractGriddedFunction, GriddedFunction, GFInterpolation
 export interpolate, argmap, argmap!, points, maxpoint, fvalues, grid
 export SubGrid, SubGriddedFunction
 
